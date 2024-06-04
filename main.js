67e6608cbfe1d8c60910c093f53a452d4f576108
Veronica <sandrinveronica@gmail.com>
2024-06-04 11:21:08 +0200

    Completato esercizio Operazioni 1

---
+++ main.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 72;
+let c = 35;
+let d = 50;
+let e = 10;
+let somma = 0;
+
+somma = (a + b + c + d + e);
+console.log('La somma tra i numeri equivale a ' + somma);
+
+
+let media = somma / 5;
+console.log('La media tra i numeri equivale a ' + media);
