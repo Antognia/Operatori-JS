f91edb56ccfb4578ebcc64679c7704c223e5213f
Veronica <sandrinveronica@gmail.com>
2024-06-04 11:41:56 +0200

    Operatori2 Selfwork

---
+++ operatori2.js
@@ -0,0 +1,7 @@
+let year = 2024;
+let birthday = 1966;
+
+let age = year - birthday;
+let difference = 100 - age;
+console.log(`Hai ${age} anni e ti mancano ${difference} anni per compierne 100`);
+
